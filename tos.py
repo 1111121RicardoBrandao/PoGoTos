3fbabef656f40fc7569e9f0b8ed33519f7fb675b
Infernoman <jjaauto@hotmail.com>
2016-08-07 19:59:19 -0400

    Update tos.py

--- tos.py
+++ tos.py
@@ -7,20 +7,26 @@ from pgoapi import PGoApi
 from pgoapi.utilities import f2i
 from pgoapi import utilities as util
 from pgoapi.exceptions import AuthException
+from pgoapi.exceptions import ServerSideRequestThrottlingException
 import pprint
 import time
 import threading
 import sys, getopt
 
 def accept_tos(username, password):
-        api = PGoApi()
-        api.set_position(40.7127837, -74.005941, 0.0)
-        api.login('ptc', username, password)
-        time.sleep(2)
-        req = api.create_request()
-        req.mark_tutorial_complete(tutorials_completed = 0, send_marketing_emails = False, send_push_notifications = False)
-        response = req.call()
-        print('Accepted Terms of Service for {}'.format(username))
+        try:
+                api = PGoApi()
+                api.set_position(40.7127837, -74.005941, 0.0)
+                api.login('ptc', username, password)
+                time.sleep(2)
+                req = api.create_request()
+                req.mark_tutorial_complete(tutorials_completed = 0, send_marketing_emails = False, send_push_notifications = False)
+                response = req.call()
+                print('Accepted Terms of Service for {}'.format(username))
+        except ServerSideRequestThrottlingException as e:
+                print('Server side throttling, Waiting 10 seconds.')
+                time.sleep(10)
+                accept_tos()
 
 with open(str(sys.argv[1])) as f:
         credentials = [x.strip().split(':') for x in f.readlines()]
